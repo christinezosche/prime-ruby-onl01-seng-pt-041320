050b5db3378e3a3f5e3bae5a5ff069c4dce07580
Christine Zosche <christine.zosche@gmail.com>
2020-04-24 03:39:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+    if number <= 3
+      return true
+    elsif number % 3 == 0
+      return false
+    elsif number % 2 == 0
+      return false
+    else
+      return true
+    end
+  end
+end
+    
