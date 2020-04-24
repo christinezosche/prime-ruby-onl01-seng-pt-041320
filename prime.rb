f346907f4f194c42c96d41d2e8085eea975daf99
Christine Zosche <christine.zosche@gmail.com>
2020-04-24 04:16:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(n)
- if n <= 0
+ if n = 2
+   return = true
+ elsif n <= 0
    return false
  elsif n = 1
    return false
- elsif n = 2
-   return true
  else
   factors = (2..n).to_a
  
