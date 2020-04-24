c7f9a800ab2faf4ef1e4efdaca6dfbdc4c8f7d14
Christine Zosche <christine.zosche@gmail.com>
2020-04-24 03:41:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,2 @@
 def prime?(n)
-    if n <= 1
-      return false
-
-    elsif n <= 3
-      return true
-    
-    else
-      i = 2
-      while i*i <= n
-        if n % i == 0
-           return false
-        i = i + 1
-      return true
-end
+ factors = [1..n*n]
